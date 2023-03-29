39177f3da8fe9fe844270fbcfd66514145308a3b
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-03-29 19:51:14 +0100

    model add user function and add post function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,10 @@ class Post extends Model
     use HasFactory;
     protected $table = 'posts';
     protected $fillable = [
-        'title', 'body', 'slug', 'image',
+        'title', 'body', 'slug', 'image','user_id'
     ];
+    public function User()
+    {
+       return $this->belongsTo(User::class);//post of user
+    }
 }
